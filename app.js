ac05e068231a3e0f6d69175227e7f5924347a88f
raymond <raymondomon@yahoo.com>
2020-08-01 19:15:53 +0100

    new functionalities added

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const yargs = require("yargs");
 const notes = require("./notes");
 
+// yargs.command({
+//   command: "add",
+//   describe: "Adding a Note",
+//   builder: {
+//     title: {
+//       describe: "Note Title",
+//       demandOption: true,
+//       type: "string",
+//     },
+//     body: {
+//       describe: "Note Body",
+//       demandOption: true,
+//       type: "string",
+//     },
+//   },
+//   handler: function (argv) {
+//     console.log("Title : ", argv.title);
+//     console.log(" Body : ", argv.body);
+//   },
+// });
+
 yargs.command({
   command: "add",
   describe: "Adding a Note",
@@ -17,8 +38,45 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("\n Title : ", argv.title);
-    console.log("\n Body : ", argv.body);
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Removing a Note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Listing a Note",
+  handler: function (argv) {
+    notes.listNotes();
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Reading a Note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.readNote(argv.title);
   },
 });
 
