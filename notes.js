3c613355b8abe8a7baa090913df176652dc7e1aa
raymond <raymondomon@yahoo.com>
2020-08-03 09:18:17 +0100

    new changes

--- notes.js
+++ notes.js
@@ -32,23 +32,6 @@ const addNote = function (title, body) {
   }
 };
 
-// const addNote = function (title, body) {
-//   const notes = loadNotes();
-//   const duplicateNotes = notes.filter(function (note) {
-//     return note.title === title;
-//   });
-//   if (duplicateNotes.length === 0) {
-//     notes.push({
-//       title: title,
-//       body: body,
-//     });
-//     saveNotes(notes);
-//     console.log("New Note Added Successfully!!!");
-//   } else {
-//     console.log("Note Title already existed!!!");
-//   }
-// };
-
 const removeNote = function (title) {
   const notes = loadNotes();
   const notesToKeep = notes.filter(function (note) {
